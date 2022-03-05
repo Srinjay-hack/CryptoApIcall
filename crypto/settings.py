e16dd488ba1f7ab70565fc7a96f6389cbd73be22
Srinjay-hack <srinjaymondal@gmail.com>
2022-03-06 00:11:05 +0530

    new

--- crypto/settings.py
+++ crypto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nq1vocbuzj05s%&w&5xhl*rkkh$-8$u+0mb88v4*6wnkw(%q-x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
